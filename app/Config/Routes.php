2a4f6a483cf3741641a016ceee8003b5bae5b04d
Dominique Foley <dfoley@bestcigarprices.com>
2023-07-08 23:48:24 -0400

    controller not found bug fixed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,9 +29,13 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+$routes->get('/', 'Home::index');
+$routes->get('/second', 'HomePage::frontpage');
 $routes->group('', ['namespace' => 'App\Controllers\Front'], function ($routes) {
-$routes->get('/', 'HomePage::index');
+
 });
+
+
 
 service('auth')->routes($routes);
 
